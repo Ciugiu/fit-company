5b1be7a5bc5bec3e590bab1fc872cb724f570d8c
ciugiu <corneliu.ciugurean@epita.fr>
2025-06-19 01:25:55 +0200

    feat: Include payment blueprint in module exports

--- src/fit/blueprints/__init__.py
+++ src/fit/blueprints/__init__.py
@@ -2,5 +2,6 @@ from .user_blueprint import user_bp
 from .auth_blueprint import auth_bp
 from .workout_blueprint import workout_bp
 from .wod_blueprint import wod_bp
+from .payment_blueprint import payment_bp
 
-__all__ = ["user_bp", "auth_bp", "workout_bp", "wod_bp"]
+__all__ = ["user_bp", "auth_bp", "workout_bp", "wod_bp", "payment_bp"]
